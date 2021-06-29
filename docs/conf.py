eb319d91176f15f83ab87dbceacdde50e556122c
Dustin Wyatt <dustin.wyatt@gmail.com>
2021-06-29 12:08:40 -0500

    set up repo buttons in docs

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,16 @@ exclude_patterns = []
 #
 html_theme = "sphinx_book_theme"
 
+html_theme_options = {
+    "repository_url": "https://github.com/dmwyatt/systa",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "repository_branch": "dev",
+    "path_to_docs": "docs/source",
+    "home_page_in_toc": True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
